a674e6e9a74b3994c9f8a74dd95a8ec89f2faeb7
Luiz Felipe <72635266+LuizNascimento442@users.noreply.github.com>
2021-05-24 10:11:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+//Carne = 400 gr por pessoa + 6 horas - 650
+//cerveja =1200 ml por pessoa + 6 horas - 200 ml
+//refrigerante/agua - 1000 ml por pessoa + 6horas 1500ml
+
+//crianças valem por 0,5
+
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular(){
+    console.log("Calculando.....");
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP()/2 * criancas);
+    let qtdTotalCerveja = cervejaPP(duracao) * adultos;
+    let qtdTotalBebidas = bebidasPP(duracao) * adultos +(bebidasPP(duracao) /2 * criancas);
+
+   
+    resultado.innerHTML =  `<p>${qtdTotalCarne/1000} Kg de Carne `;
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja/355)} Latas de Cerveja `;
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas/2000)} Pets's de 2l Bebida `;
+
+
+}
+
+function carnePP(duracao){
+
+    if(duracao >=6){
+        return 650;
+    }else{
+        return 400;
+    }
+
+}
+
+function cervejaPP(duracao){
+
+    if(duracao >=6){
+        return 2000;
+    }else{
+        return 1200;
+    }
+
+}
+
+function bebidasPP(duracao){
+
+    if(duracao >=6){
+        return 1500;
+    }else{
+        return 1000;
+    }
+
+}
+
+
